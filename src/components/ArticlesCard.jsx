1bde7b0c052b30205bfeca89082833912c20de14
maximusMD <maximilian.dunne@gmail.com>
2023-10-23 15:13:01 +0100

    Articles List completed (Ticket 4), no styling

--- src/components/ArticlesCard.jsx
+++ src/components/ArticlesCard.jsx
@@ -1,5 +1,15 @@
-export default function ArticlesCard() {
+export default function ArticlesCard({ArticlesName, ArticlesAuthor, ArticlesTopic, ArticlesCreatedAt, ArticlesVotes, ArticlesImg, ArticlesCommentCount}) {
     return (
-        <p>ArticlesCard</p>
+        <div className="ArticlesCard">
+        <h3 className="ArticlesName">{ArticlesName}</h3>
+        <ul>
+            <li>{ArticlesAuthor}</li>
+            <li>{ArticlesTopic}</li>
+            <li>{ArticlesCreatedAt}</li>
+            <li>{ArticlesVotes}</li>
+            <img src={ArticlesImg}/> 
+            <li>{ArticlesCommentCount}</li>
+        </ul>
+    </div>
     )
 }
