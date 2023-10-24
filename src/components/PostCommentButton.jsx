bef6fb2a1dd7fe69996f2c31db20f5e000005028
maximusMD <maximilian.dunne@gmail.com>
2023-10-24 15:52:21 +0100

    This commit will add the functionality to post a comment

--- src/components/PostCommentButton.jsx
+++
@@ -1,5 +0,0 @@
-export default function PostCommentButton() {
-    return (
-        <p>PostCommentButton</p>
-    )
-}
--- src/components/PostCommentContainer.jsx
+++ src/components/PostCommentContainer.jsx
@@ -1,5 +1,45 @@
-export default function PostCommentContainer() {
+import axios from "axios";
+import PostCommentForm from "./PostCommentForm";
+import { useState } from "react";
+
+export default function PostCommentContainer({ article_id }) {
+    const [isPosting, setPosting] = useState(false)
+    const [postSuccess, setPostSuccess] = useState(false)
+    const [postError, setPostError] = useState(null)
+    const [comment, setComment] = useState(null)
+
+    const handleCommentSubmit = async (comment) => {
+        console.log(comment);
+        setPosting(true)
+        try {
+            const res = await axios.post(
+            `https://nc-news-31tf.onrender.com/api/articles/${article_id}/comments`,
+            comment, 
+            {
+            headers: {
+                "Content-Type": "application/json",
+                },
+            }
+        )
+        setPostSuccess(true)
+        setPostError(null)
+        } 
+        catch (error) {
+        setPostError("Error - Please try again.")
+        console.log(error);
+        console.error("Error posting comment:", error)
+        } 
+        finally {
+        setPosting(false)
+        }
+    }  
     return (
-        <p>PostCommentContainer</p>
+        <div>
+        {isPosting ? <p>Posting your comment...</p> : null}
+        {postSuccess ? <p>Comment posted successfully!</p> : null}
+        {postError ? <p>Error posting comment: {postError}</p> : null}
+        <PostCommentForm onCommentSubmit={handleCommentSubmit} setComment={setComment} />
+        {comment && console.log(comment)}
+        </div>
     )
 }
