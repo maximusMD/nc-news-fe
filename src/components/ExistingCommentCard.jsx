412854276e0d3514d4d63e8e027b0ef9f80e1a35
maximusMD <maximilian.dunne@gmail.com>
2023-10-26 16:57:27 +0100

    This commit will add completed deletion functionality

--- src/components/ExistingCommentCard.jsx
+++ src/components/ExistingCommentCard.jsx
@@ -1,11 +1,36 @@
-export default function ExistingCommentCard({CommentsVotes, CommentsCreatedAt, CommentsAuthor, CommentsBody, CommentsTopic}) {
+import axios from "axios";
+
+export default function ExistingCommentCard({CommentsVotes, CommentsCreatedAt, CommentsAuthor, CommentsBody, CommentsTopic, comment_id, user, updateComments}) {
+    const handleDeleteComment = async () => {
+        console.log("CommentsAuthor:", CommentsAuthor);
+        console.log("user:", user)
+        if (CommentsAuthor !== user) {
+        alert("Na na na can't touch this!")
+        return
+        }
+
+    try {
+        await axios.delete(`https://nc-news-31tf.onrender.com/api/comments/${comment_id}`)
+        updateComments(comment_id)
+        alert("Comment deleted successfully!")
+    } 
+    catch (error) {
+        console.error("Error deleting comment:", error)
+    }
+    }
     return (
-        <div className="ExistingCommentCard">
-            <p>{CommentsBody}</p>
-            <p>{CommentsAuthor}</p>
-            <p>{CommentsVotes}</p>
-            <p>{CommentsCreatedAt}</p>
-            <p>{CommentsTopic}</p>
+    <div className="ExistingCommentCard">
+        <p>{CommentsBody}</p>
+        <p>{CommentsAuthor}</p>
+        <p>{CommentsVotes}</p>
+        <p>{CommentsCreatedAt}</p>
+        <p>{CommentsTopic}</p>
+        {CommentsAuthor === user && (
+        <div>
+            <button onClick={handleDeleteComment}>Delete</button>
         </div>
-    )
+        )}
+
+    </div>
+    );
 }
