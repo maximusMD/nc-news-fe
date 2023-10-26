95854e863458b4fe5c1ed51759239d4632ea5b06
maximusMD <maximilian.dunne@gmail.com>
2023-10-26 15:47:59 +0100

    Styling changes

--- src/components/ExistingCommentCard.jsx
+++ src/components/ExistingCommentCard.jsx
@@ -1,11 +1,10 @@
 export default function ExistingCommentCard({CommentsVotes, CommentsCreatedAt, CommentsAuthor, CommentsBody, CommentsTopic}) {
     return (
         <div className="ExistingCommentCard">
-        <h3 className="CommentName">Comment</h3>
+            <p>{CommentsBody}</p>
+            <p>{CommentsAuthor}</p>
             <p>{CommentsVotes}</p>
             <p>{CommentsCreatedAt}</p>
-            <p>{CommentsAuthor}</p>
-            <p>{CommentsBody}</p>
             <p>{CommentsTopic}</p>
         </div>
     )
